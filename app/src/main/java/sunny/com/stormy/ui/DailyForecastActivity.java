884108f6e3985ee2081a193a0ca6c6e043c1c71c
Sunny Raj <sundarcs@gwu.edu>
2015-11-28 22:39:47 -0500

    Replace default with custom adapter

--- app/src/main/java/sunny/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/sunny/com/stormy/ui/DailyForecastActivity.java
@@ -10,18 +10,18 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 
 import sunny.com.stormy.R;
+import sunny.com.stormy.adapters.DayAdapter;
+import sunny.com.stormy.weather.Day;
 
 public class DailyForecastActivity extends ListActivity {
 
+    private Day[] mDays;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
-        setListAdapter(adapter);
-
+        DayAdapter adapter = new DayAdapter(this, mDays);
 
     }
 
