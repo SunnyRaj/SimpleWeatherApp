5743b4949ac986a03bb9fe56290afff03065471b
Sunny Raj <sundarcs@gwu.edu>
2015-11-29 13:12:57 -0500

    Add methods for RecyclerView adapter

--- app/src/main/java/sunny/com/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/sunny/com/stormy/ui/HourlyForecastActivity.java
@@ -6,11 +6,15 @@ import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.util.Arrays;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import sunny.com.stormy.R;
 import sunny.com.stormy.adapters.HourAdapter;
 import sunny.com.stormy.weather.Hour;
@@ -19,17 +23,25 @@ public class HourlyForecastActivity extends AppCompatActivity {
 
     private Hour[] mHours;
 
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
 
-        //HourAdapter adapter = new HourAdapter(this, mHours);
-        // setListAdapter(adapter);
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
     }
 
 }
