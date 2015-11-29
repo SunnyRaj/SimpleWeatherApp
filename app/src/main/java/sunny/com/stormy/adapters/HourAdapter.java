414dfd3092ca57592b3c6fc971aa245715b83f29
Sunny Raj <sundarcs@gwu.edu>
2015-11-29 11:23:12 -0500

    Add Recycler View holder

--- app/src/main/java/sunny/com/stormy/adapters/HourAdapter.java
+++ app/src/main/java/sunny/com/stormy/adapters/HourAdapter.java
@@ -1,7 +1,47 @@
 package sunny.com.stormy.adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import sunny.com.stormy.R;
+
 /**
  * Created by sunny on 11/29/15.
  */
-public class HourAdapter {
+public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
+
+    @Override
+    public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(HourViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class HourViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTimeLabel;
+        public TextView mSummaryLabel;
+        public TextView mTemperatureLabel;
+        public ImageView mIconImageView;
+
+        public HourViewHolder(View itemView) {
+            super(itemView);
+
+            mTimeLabel = (TextView) itemView.findViewById(R.id.timeLabel);
+            mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
+            mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
+            mIconImageView = (ImageView) itemView.findViewById(R.id.iconImageView);
+        }
+    }
 }
