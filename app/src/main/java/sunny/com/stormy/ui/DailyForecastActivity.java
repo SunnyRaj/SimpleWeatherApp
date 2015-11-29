a712634cb440835fcc977a75a33a3dcf4c1bfde4
Sunny Raj <sundarcs@gwu.edu>
2015-11-28 22:37:57 -0500

    Set the data for adapter

--- app/src/main/java/sunny/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/sunny/com/stormy/ui/DailyForecastActivity.java
@@ -22,6 +22,7 @@ public class DailyForecastActivity extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
         setListAdapter(adapter);
 
+
     }
 
 }
